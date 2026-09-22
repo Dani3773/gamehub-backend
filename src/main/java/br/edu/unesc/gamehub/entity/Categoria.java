3fa381b5afd395b44f00e0c49c34c730669da98b
Igor <igorraiciky@gmail.com>
2026-09-22 09:25:03 -0300

    feat: adiciona CRUD de categorias

---
+++ src/main/java/br/edu/unesc/gamehub/entity/Categoria.java
@@ -0,0 +1,36 @@
+package br.edu.unesc.gamehub.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+        name = "categoria",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_categoria_nome",
+                        columnNames = "nome"
+                )
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nome;
+
+    @Column(length = 500)
+    private String descricao;
+}
---
+++ src/main/java/br/edu/unesc/gamehub/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package br.edu.unesc.gamehub.repository;
+
+import br.edu.unesc.gamehub.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    Optional<Categoria> findByNome(String nome);
+
+    boolean existsByNome(String nome);
+}
